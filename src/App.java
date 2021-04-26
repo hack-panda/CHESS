2f83652cc2dd9dca2ab13077a4d2c07d9e97075e
hack-panda <dthesec@gmail.com>
2021-04-26 10:10:19 +0530

    Created SetUp for the Project
    Created all the necessary files for the project

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+    }
+}
